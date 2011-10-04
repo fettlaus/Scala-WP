4ca0ead913d7fa8f4676fce0a4959bcd4346acbf
morfu <xmorfu@googlemail.com>
2011-10-04 23:41:45 +0200

    Added a recursive and an iterative method to compute the Collatz conjecture.

--- Aufgabe 2/src/de/haw/scala/A2/T1/Aufgabe_1.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T1/Aufgabe_1.scala
@@ -2,6 +2,25 @@ package de.haw.scala.A2.T1
 
 object Aufgabe_1 {
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]) {
+    println(collatzIterative(23).mkString("Iterative: [", ", ", "]"))
+    println(collatz(23).mkString("Recursive: [", ", ", "]"))
+  }
 
+  def collatz(n:Int): Array[Int] = {
+    if (n == 1) Array(1)
+    else if (n%2 == 0) n +: collatz(n/2)
+    else n +: collatz(n*3+1)
+  }
+  
+  def collatzIterative(n: Int): Array[Int] = {
+    var arr = Array[Int]()
+    var i = n
+    while (i != 1) {
+      arr = arr :+ i
+      if (i%2 == 0) i = i/2
+      else i = i*3+1
+    }
+    arr :+ i
+  }
 }
