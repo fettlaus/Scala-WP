5d78ecc9ae14a38c13fc629c7d360905ea94400d
morfu <xmorfu@googlemail.com>
2011-10-05 12:57:26 +0200

    Added "Aufgabe Ex4", tests with class Person.

---
+++ Aufgabe 1/src/de/haw/scala/A1/Ex4/PersonTest.scala
@@ -0,0 +1,15 @@
+package de.haw.scala.A1.Ex4
+
+object PersonTest {
+
+  def main(args: Array[String]) { 
+    val a = Array(new Person("She-Ra"), new Person("He-Man"), new Person("Skeletor"))
+    println(a.mkString("Array(", ",", ")"))
+    a(1).address = "Grayskull"
+    println(a.mkString("Array(", ",", ")"))
+  }
+
+  class Person(val name: String, var address: String = "HH", var age: Int = 30) {
+    override def toString = "Person(" + name + "," + address + "," + age + ")"
+  }
+}
