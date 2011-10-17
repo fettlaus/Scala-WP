9e25f2e84d39f8961678da4e1ff294b67cde28ab
Arne Wischer <Fettlaus@gmail.com>
2011-10-17 02:24:02 +0000

    added source removed return value

--- Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
@@ -1,10 +1,12 @@
 package de.haw.scala.A2.T4
 
+// some sources:
 // http://dotneverland.blogspot.com/2009/09/quick-sample-of-lists-and-tuples-in.html
 // http://stackoverflow.com/questions/2627919/scala-how-can-i-sort-an-array-of-tuples-by-their-second-element
+// http://alan.dipert.org/post/307586762/polyglot-folding-ruby-clojure-scala
 
 object Rundreise {
-  def distance(c1:Int,c2:Int):Int=100/(if(c1>c2)(c1-c2)else(c2-c1))
+  def distance(c1:Int,c2:Int)=100/(if(c1>c2)(c1-c2)else(c2-c1))
   
   def main(args: Array[String]): Unit = {  }
   val l = List(1,2,3,4)
