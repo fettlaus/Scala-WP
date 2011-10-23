2ab93a330dfcdccc4e724d8d7b4f20fab7599ce8
Arne Wischer <Fettlaus@gmail.com>
2011-10-23 16:47:31 +0000

    replaced foldLeft with reduceLeft

--- Aufgabe 2/src/de/haw/scala/A2/T3/PolynomMain.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T3/PolynomMain.scala
@@ -2,7 +2,7 @@ package de.haw.scala.A2.T3
 
 object PolynomMain {
 
-  def polynom(x:Integer,a:Integer*)= a.reverse.foldLeft(0)((a,b)=>a*x+b)
+  def polynom(x:Integer,a:Integer*)= a.reverse.reduceLeft((a,b)=>a*x+b)
   
   def main(args: Array[String]): Unit = {  
     println(polynom(2,1,2,3,4,5))
