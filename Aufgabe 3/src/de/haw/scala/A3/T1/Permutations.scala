aff67ca2e6a8952f6c2c2be9ca0a886287ea1f3f
Arne Wischer <Fettlaus@gmail.com>
2011-10-24 00:01:21 +0000

    added companion object added comments added NaP added equals

--- Aufgabe 3/src/de/haw/scala/A3/T1/Permutations.scala
+++ Aufgabe 3/src/de/haw/scala/A3/T1/Permutations.scala
@@ -1,10 +1,37 @@
 package de.haw.scala.A3.T1
 
+/**
+ * @author Arne Wischer <Fettlaus@gmail.com>
+ *
+ * Sources:
+ * http://daily-scala.blogspot.com/2009/11/varargs.html
+ * http://fupeg.blogspot.com/2009/04/tail-recursion-in-scala.html
+ * http://www.scala-lang.org/api/current/scala/collection/immutable/Vector$.html
+ */
 object Permutations {
-  case class s(elm:Int*){
-    def apply(num:Int) = elm(num-1)    
-    override def toString = "s("+elm.mkString(",")+")"
+  object NaP extends s{
+    override def toString = "NaP"
+      override def apply(x:Int) = -1
+  }
+  object s{
+    def apply(elm:Int){ this(elm)}
+    def apply(elms:Int*) = {
+     if(elms.distinct.sorted!=elms.sorted)
+       NaP
+     else
+       new s(elms:_*)
+    }
+  }
+  class s(val elm:Int*){
+    def apply(num:Int) = elm(num-1)
     
+    override def toString = "s("+elm.mkString(",")+")"
+    override def equals(in:Any)={
+      in match{
+        case k:s if(k.elm==elm) => true
+        case _=> false
+      }
+    }
     def cycles = {      
       val r = for(i<-Vector(elm:_*)) // kein toVector :(        
         yield {
--- Aufgabe 3/src/de/haw/scala/A3/T1/PermutationsMain.scala
+++ Aufgabe 3/src/de/haw/scala/A3/T1/PermutationsMain.scala
@@ -15,17 +15,20 @@ object PermutationsMain {
     println(s1 cycles) // Vector(List(1,2,4),List(3,5))
     println(s3 cycles) // Vector(List(1,2,4,5,3))
     println(s(1,2,3,4,5) fixPoints) // List(1,2,3,4,5)
-    /*
     val err1= s(2,5,8,5,4,6,3) 
-    val err2= s(2,5,8,5,4,6,3,1) 
+    val err2= s(2,5,8,5,4,6,3,1)
     println(err1) // NaP
     println(err2) // NaP
+    
     println(NaP cycles) // Vector(List())
+    
     println(NaP fixPoints) // List()
+    
     println(NaP(10)) // -1
-    def foo (f: Int => Int, g: Int => Double, x: Int) = g(f(x)) 
-    println(foo(s(1,6,4,2,3,5), Array(1. 0,-1.0,1,4. 0,-3.0), 4)) //1.0
-    */
+    
+  //  def foo (f: Int => Int, g: Int => Double, x: Int) = g(f(x)) 
+  //  println(foo(s(1,6,4,2,3,5), Array(1.0,-1.0,1,4.0,-3.0), 4)) //1.0
+    
   }
 
 }
