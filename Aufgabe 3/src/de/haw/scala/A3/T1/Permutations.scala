ed8e57423c2375dc9b5de9286db0a5ceb6fe083d
Arne Wischer <Fettlaus@gmail.com>
2011-10-24 00:07:09 +0000

    formatted code added comment

--- Aufgabe 3/src/de/haw/scala/A3/T1/Permutations.scala
+++ Aufgabe 3/src/de/haw/scala/A3/T1/Permutations.scala
@@ -9,48 +9,48 @@ package de.haw.scala.A3.T1
  * http://www.scala-lang.org/api/current/scala/collection/immutable/Vector$.html
  */
 object Permutations {
-  object NaP extends s{
+  object NaP extends s {
     override def toString = "NaP"
-      override def apply(x:Int) = -1
+    override def apply(x: Int) = -1
   }
-  object s{
-    def apply(elm:Int){ this(elm)}
-    def apply(elms:Int*) = {
-     if(elms.distinct.sorted!=elms.sorted)
-       NaP
-     else
-       new s(elms:_*)
+  object s {
+    def apply(elm: Int) { this(elm) }
+    def apply(elms: Int*) = {
+      if (elms.distinct.sorted != elms.sorted)
+        NaP
+      else
+        new s(elms: _*)
     }
   }
-  class s(val elm:Int*){
-    def apply(num:Int) = elm(num-1)
-    
-    override def toString = "s("+elm.mkString(",")+")"
-    override def equals(in:Any)={
-      in match{
-        case k:s if(k.elm==elm) => true
-        case _=> false
+  class s(val elm: Int*) {
+    def apply(num: Int) = elm(num - 1)
+
+    override def toString = "s(" + elm.mkString(",") + ")"
+    override def equals(in: Any) = {
+      in match {
+        case k: s if (k.elm == elm) => true
+        case _ => false
       }
     }
-    def cycles = {      
-      val r = for(i<-Vector(elm:_*)) // kein toVector :(        
-        yield {
-        def cy(k:Int,l:List[Int]):List[Int]={
-          if(this(k)==i)
+    def cycles = {
+      val r = for (i <- Vector(elm: _*)) // kein toVector :(        
+      yield {
+        def cy(k: Int, l: List[Int]): List[Int] = {
+          if (this(k) == i)
             l
           else
-            this(k)::cy(this(k),l)
+            this(k) :: cy(this(k), l)
+        }
+        cy(i, i :: List()).sorted
       }
-        cy(i,i::List()).sorted
-      }      
       r.distinct
     }
-    
+
     def fixPoints = {
-      val k = for(i<-elm.toIterable;if(this(i)==i))
-            yield i
+      val k = for (i <- elm.toIterable; if (this(i) == i))
+        yield i
       k.toList
     }
-}
-    
+  }
+
 }
--- Aufgabe 3/src/de/haw/scala/A3/T1/PermutationsMain.scala
+++ Aufgabe 3/src/de/haw/scala/A3/T1/PermutationsMain.scala
@@ -1,34 +1,38 @@
 package de.haw.scala.A3.T1
 
+/**
+ * @author Arne Wischer <Fettlaus@gmail.com>
+ *
+ */
 object PermutationsMain {
 
-  def main(args: Array[String]): Unit = {  
-    import Permutations. _ 
-    val s1= s(2,4,5,1,3)
-    val s2= s(2,4,5,1,3) 
-    val s3= s(2,4,1,5,3) 
+  def main(args: Array[String]): Unit = {
+    import Permutations._
+    val s1 = s(2, 4, 5, 1, 3)
+    val s2 = s(2, 4, 5, 1, 3)
+    val s3 = s(2, 4, 1, 5, 3)
     println(s1) // s(2,4,5,1,3)
-    println(s1==s2) // true
-    println(s1==s3) // false   
+    println(s1 == s2) // true
+    println(s1 == s3) // false   
     println(s1(3)) // 5
-    println(s3(3))  // 1
+    println(s3(3)) // 1
     println(s1 cycles) // Vector(List(1,2,4),List(3,5))
     println(s3 cycles) // Vector(List(1,2,4,5,3))
-    println(s(1,2,3,4,5) fixPoints) // List(1,2,3,4,5)
-    val err1= s(2,5,8,5,4,6,3) 
-    val err2= s(2,5,8,5,4,6,3,1)
+    println(s(1, 2, 3, 4, 5) fixPoints) // List(1,2,3,4,5)
+    val err1 = s(2, 5, 8, 5, 4, 6, 3)
+    val err2 = s(2, 5, 8, 5, 4, 6, 3, 1)
     println(err1) // NaP
     println(err2) // NaP
-    
+
     println(NaP cycles) // Vector(List())
-    
+
     println(NaP fixPoints) // List()
-    
+
     println(NaP(10)) // -1
-    
-  //  def foo (f: Int => Int, g: Int => Double, x: Int) = g(f(x)) 
-  //  println(foo(s(1,6,4,2,3,5), Array(1.0,-1.0,1,4.0,-3.0), 4)) //1.0
-    
+
+    //  def foo (f: Int => Int, g: Int => Double, x: Int) = g(f(x)) 
+    //  println(foo(s(1,6,4,2,3,5), Array(1.0,-1.0,1,4.0,-3.0), 4)) //1.0
+
   }
 
 }
