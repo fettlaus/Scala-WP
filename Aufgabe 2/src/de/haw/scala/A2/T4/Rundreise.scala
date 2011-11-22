5db11f0d804455cb11436d51ebceb0c69bcad90b
Fettlaus <Fettlaus@gmail.com>
2011-11-22 08:45:31 +0000

    fix main

--- Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
@@ -8,9 +8,10 @@ package de.haw.scala.A2.T4
 object Rundreise {
   def distance(c1:Int,c2:Int)=100/(if(c1>c2)(c1-c2)else(c2-c1))
   
-  def main(args: Array[String]): Unit = {  }
+  def main(args: Array[String]): Unit = {
   val l = List(1,2,3,4)
   val k = for(i<-l.permutations)
     yield i.zip(i.tail).foldLeft(List[Int]())((list,b)=>distance(b._1,b._2)::list).sum
   println(l.permutations.zip(k).toList.sortBy(_._2).head)
+  }
 }
