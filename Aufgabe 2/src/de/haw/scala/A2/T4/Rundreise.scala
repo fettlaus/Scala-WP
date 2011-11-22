06404871f8cceac1a11e980459bdefc3707546ab
Fettlaus <Fettlaus@gmail.com>
2011-11-22 08:46:13 +0000

    formatted code

--- Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T4/Rundreise.scala
@@ -6,12 +6,12 @@ package de.haw.scala.A2.T4
 // http://alan.dipert.org/post/307586762/polyglot-folding-ruby-clojure-scala
 
 object Rundreise {
-  def distance(c1:Int,c2:Int)=100/(if(c1>c2)(c1-c2)else(c2-c1))
-  
+  def distance(c1: Int, c2: Int) = 100 / (if (c1 > c2) (c1 - c2) else (c2 - c1))
+
   def main(args: Array[String]): Unit = {
-  val l = List(1,2,3,4)
-  val k = for(i<-l.permutations)
-    yield i.zip(i.tail).foldLeft(List[Int]())((list,b)=>distance(b._1,b._2)::list).sum
-  println(l.permutations.zip(k).toList.sortBy(_._2).head)
+    val l = List(1, 2, 3, 4)
+    val k = for (i <- l.permutations)
+      yield i.zip(i.tail).foldLeft(List[Int]())((list, b) => distance(b._1, b._2) :: list).sum
+    println(l.permutations.zip(k).toList.sortBy(_._2).head)
   }
 }
