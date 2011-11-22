50c07b952a20408b537852a5bb02fc8f27dc271d
Fettlaus <Fettlaus@gmail.com>
2011-11-22 09:01:58 +0000

    changed encoding to utf-8

--- Aufgabe 2/src/de/haw/scala/A2/T2/Position.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T2/Position.scala
@@ -3,7 +3,7 @@ package de.haw.scala.A2.T2
 class Position(val x:Integer = 0,val y:Integer = 0) {
   def this(x: Integer) = this(x,0)
   if (x<0||y<0)
-    throw new IllegalArgumentException("x und y m�ssen gr��er/gleich Null sein!")
+    throw new IllegalArgumentException("x und y mÃ¼ssen grÃ¶Ãer/gleich Null sein!")
   override def toString = "Position("+x+","+y+")"
 }
 
