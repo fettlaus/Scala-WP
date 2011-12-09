51cefdf6aa813e1f9ec703ea7a31400f815cd4f7
Arne Wischer <Fettlaus@gmail.com>
2011-12-09 16:05:31 +0100

    first part

---
+++ Aufgabe 5/src/de/haw/scala/A5/T2/EitherListMain.scala
@@ -0,0 +1,19 @@
+package de.haw.scala.A5.T2
+
+object EitherListMain {
+
+  def main(args: Array[String]): Unit = {  
+    val customers = List(Left(Person("P1")),Left(Person("P2")),
+    					Right(Company(Person("CEO1"))), Left(Person("P3")),
+    					Right(Company(Person("CEO2"))),Right(Company(Person("CEO3"))),
+    					Left(Person("P3")))
+	println(customers);
+    println(customers.partition(_.isLeft))
+    println(customers.flatMap(_.left.toSeq))
+    println(customers.flatMap(_.right.toSeq))
+  }
+  
+  case class Company(person:Person)
+  case class Person(name:String)
+
+}
