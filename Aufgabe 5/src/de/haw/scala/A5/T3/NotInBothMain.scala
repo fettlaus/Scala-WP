fcde94d4b403da2f48a2f7731f0eaf5c7db19977
Arne Wischer <Fettlaus@gmail.com>
2011-12-09 18:12:24 +0100

    removed non-tailrec solution

--- Aufgabe 5/src/de/haw/scala/A5/T3/NotInBothMain.scala
+++ Aufgabe 5/src/de/haw/scala/A5/T3/NotInBothMain.scala
@@ -5,7 +5,11 @@ object NotInBothMain {
   def main(args: Array[String]): Unit = {
     println(notInBoth(List(1,2,5),List(7,2,3)))
   }
-  
+  /*
+   * simple solution but not tailrec
   def notInBoth[T](a1: Seq[T], a2: Seq[T]):Seq[T]= a1.diff(a2)++a2.diff(a1)
-
+  */
+  def notInBoth[T](a1: Seq[T], a2: Seq[T]):Seq[T]={
+    a1
+  }
 }
