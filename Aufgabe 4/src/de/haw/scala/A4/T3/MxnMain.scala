5d65827357831d89fca32fd085884110f0c5a68d
Arne Wischer <Fettlaus@gmail.com>
2011-12-09 20:06:07 +0100

    added scalala lib

--- Aufgabe 4/src/de/haw/scala/A4/T3/MxnMain.scala
+++ Aufgabe 4/src/de/haw/scala/A4/T3/MxnMain.scala
@@ -2,6 +2,9 @@ package de.haw.scala.A4.T3
 
 object MxnMain {
 
-  def main(args: Array[String]): Unit = {  }
+  def main(args: Array[String]): Unit = {
+    import scalala.tensor.dense._
+    val m = DenseMatrix.zeros[Int](5,5)
+  }
 
 }
