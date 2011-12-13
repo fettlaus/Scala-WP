d7321eec0a847b8a8fda44dab79f913f2b843989
Arne Wischer <Fettlaus@gmail.com>
2011-12-13 02:21:42 +0000

    replaced Integers with Ints

--- Aufgabe 2/src/de/haw/scala/A2/T2/Position.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T2/Position.scala
@@ -1,12 +1,12 @@
 package de.haw.scala.A2.T2
 
-class Position(val x:Integer = 0,val y:Integer = 0) {
-  def this(x: Integer) = this(x,0)
+class Position(val x:Int = 0,val y:Int = 0) {
+  def this(x: Int) = this(x,0)
   if (x<0||y<0)
     throw new IllegalArgumentException("x und y mÃ¼ssen grÃ¶Ãer/gleich Null sein!")
   override def toString = "Position("+x+","+y+")"
 }
 
 object Position{
-  def apply(x:Integer,y:Integer) = new Position(x,y)
+  def apply(x:Int,y:Int) = new Position(x,y)
 }
