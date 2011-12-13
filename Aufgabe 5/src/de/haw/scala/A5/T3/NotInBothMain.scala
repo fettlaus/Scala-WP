c61b5fda80bf27ad7ba288f358e84d1192543450
Arne Wischer <Fettlaus@gmail.com>
2011-12-13 02:44:49 +0000

    added (not working) code by Christian Braun

--- Aufgabe 5/src/de/haw/scala/A5/T3/NotInBothMain.scala
+++ Aufgabe 5/src/de/haw/scala/A5/T3/NotInBothMain.scala
@@ -9,7 +9,19 @@ object NotInBothMain {
    * simple solution but not tailrec
   def notInBoth[T](a1: Seq[T], a2: Seq[T]):Seq[T]= a1.diff(a2)++a2.diff(a1)
   */
-  def notInBoth[T](a1: Seq[T], a2: Seq[T]):Seq[T]={
-    a1
+  def notInBoth[T](a1: Seq[T], a2: Seq[T]) = {
+    import scala.annotation.tailrec
+    @tailrec
+    def myFold[T](map: Map[T, Boolean], list: Seq[T]): Map[T, Boolean] = {
+        if (list.isEmpty) 
+          map
+        else 
+          myFold(map.updated(list.head, !map.contains(list.head)), list.tail)
+    }
+ 
+    myFold(collection.immutable.HashMap(a1.map((_, true)).toList: _*), a2) collect{ 
+          case (element, in) => element 
+      } toList
   }
+  
 }
