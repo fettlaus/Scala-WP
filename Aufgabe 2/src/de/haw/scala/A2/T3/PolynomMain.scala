b31c360a3258a69cf87570dada7a51f9bf7d2fec
Arne Wischer <Fettlaus@gmail.com>
2011-12-13 02:22:24 +0000

    replaced Integers with Ints

--- Aufgabe 2/src/de/haw/scala/A2/T3/PolynomMain.scala
+++ Aufgabe 2/src/de/haw/scala/A2/T3/PolynomMain.scala
@@ -2,7 +2,7 @@ package de.haw.scala.A2.T3
 
 object PolynomMain {
 
-  def polynom(x:Integer,a:Integer*)= a.reduceLeft((a,b)=>a*x+b)
+  def polynom(x:Int,a:Int*)= a.reduceLeft((a,b)=>a*x+b)
   
   def main(args: Array[String]): Unit = {  
     println(polynom(2,1,2,3,4,5))
