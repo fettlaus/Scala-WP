8b6ad838a8370e0334de7072641e77dbb7283728
Arne Wischer <Fettlaus@gmail.com>
2012-01-08 20:46:22 +0000

    finished visuals and clicks

--- Vortrag/src/scala_ex/controller/Controller.scala
+++ Vortrag/src/scala_ex/controller/Controller.scala
@@ -1,10 +1,33 @@
 package scala_ex.controller
 import scala_ex.view._
 import scala_ex.model._
+import scala.swing.event.MouseEntered
+import scala.swing.event.MouseExited
+import scala.swing.event.MouseClicked
+import scala.swing.event.MouseReleased
+import scala.swing.event.MousePressed
+import scala.swing.event.ButtonClicked
+import scala.swing.event.ButtonClicked
 
 
 class Controller(name:String) {
   val view = new View(name)
   val model = new Model
+  val m = view.mainpanel
+  
+  m.listenTo(m.mouse.moves,m.mouse.clicks)
+  m.reactions +={
+          case e:MouseEntered => view.mouseoutput(1).text = (view.mouseoutput(1).text.toInt+1).toString
+          case e:MouseExited => view.mouseoutput(2).text = (view.mouseoutput(2).text.toInt+1).toString
+          case e:MouseClicked => view.mouseoutput(0).text = (view.mouseoutput(0).text.toInt+1).toString
+          case e:MousePressed => view.mouseoutput(3).text = (view.mouseoutput(3).text.toInt+1).toString
+          case e:MouseReleased => view.mouseoutput(4).text = (view.mouseoutput(4).text.toInt+1).toString
+        }
+  
+  view.listenTo(view.calc)
+  view.reactions+={
+          case ButtonClicked(b) => println("butt")
+        }
+  
 
 }
--- Vortrag/src/scala_ex/view/View.scala
+++ Vortrag/src/scala_ex/view/View.scala
@@ -8,27 +8,43 @@ import scala.swing.Label
 import scala.swing.TextField
 import scala.swing.GridPanel
 import java.awt.Dimension
-import java.awt.BorderLayout
 import scala.swing.TextArea
+import scala.swing.event.MouseEvent
+import scala.swing.event.MouseEntered
+import scala.swing.event.MouseEntered
+import scala.swing.event.MouseExited
+import scala.swing.event.MouseClicked
+import scala.swing.event.MouseReleased
+import scala.swing.event.MousePressed
+import scala.swing.Reactions
+import scala.swing.Panel
+import scala.swing.ScrollPane
 
-class View(name:String) extends Frame {
-  private val mouseoutput = Array.fill(5)(new Label("0"))
-    title = name   
-    val panel = new BorderPanel{
+class View(name:String) extends Frame{
+  val mouseoutput = Array.fill(5)(new Label("0"))
+  private val mouselabeltext = Array("Mouse Click:","Mouse Enter:","Mouse Exit","Mouse Pressed:", "Mouse Relesed:" )
+  val input = new TextField(25)
+  val result = new TextArea("Gefundene Primzahlen:\n")
+  val progress = new Label()
+  val calc = new Button("Finde")
+  title = name
+ 
+    val mainpanel = new BorderPanel {
+    
       // Top (North)
       add(new FlowPanel{
-        contents += new Label("Finde Primzahlen bis:")
-        contents += new TextField(9)
-        contents += new Button("Finde")
+        contents += new Label("Finde alle Primzahlen bis:")
+        contents += input
+        contents += calc
       },BorderPanel.Position.North)
       
       // Left (West)
-      add(new GridPanel(2,1){
+      val d = add(new GridPanel(2,1){
         contents += new Label("Maus Events")
         contents += new GridPanel(5,2){
-          for(m <- mouseoutput){
-            contents += new Label("Hallo")
-            contents += m
+          for(i <- 0 to mouseoutput.length-1){
+            contents += new Label(mouselabeltext(i))
+            contents += mouseoutput(i)
           }
         }
       },BorderPanel.Position.West)
@@ -39,14 +55,9 @@ class View(name:String) extends Frame {
         },BorderPanel.Position.East)
         
         // Center
-      add(new FlowPanel{
-        contents += new TextArea("Gefundene Primzahlen:\n"){
-          lineWrap = true
-          columns = 40
-          rows = 40
-          editable = false
-          text += " 2,3,5,7,...\n"
-        }
+      add(new BorderPanel{
+        result.lineWrap = true
+        add(new ScrollPane(result),BorderPanel.Position.Center)      
       },BorderPanel.Position.Center)
       
       // Bottom (South)
@@ -54,11 +65,11 @@ class View(name:String) extends Frame {
         contents += new Label("Aktuelle Zahl:")
         contents += new Label("erlege gerade Zahl XYZ")
       },BorderPanel.Position.South)
-      
-      
     }
+  
+  
   preferredSize = new Dimension(640,480)
   peer.setLocation(50,50)
-    contents = panel
+  contents = mainpanel
     visible = true
 }
